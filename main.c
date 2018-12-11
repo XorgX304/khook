aeb5edd65ddb0a04119bb43569ed2659cdaaba7c
Ilya V. Matveychikov <matvejchikov@gmail.com>
2018-12-11 09:39:42 +0000

    test

--- main.c
+++ main.c
@@ -8,41 +8,11 @@
 // An example of using KHOOK
 ////////////////////////////////////////////////////////////////////////////////
 
-#include <linux/fs.h>
-
-KHOOK(inode_permission);
-static int khook_inode_permission(struct inode *inode, int mask)
+KHOOK_EXT(int, devmem_is_allowed, unsigned long);
+static int khook_devmem_is_allowed(unsigned long pagenr)
 {
-	int ret = 0;
-
-	KHOOK_GET(inode_permission);
-	ret = KHOOK_ORIGIN(inode_permission, inode, mask);
-	printk("%s(%p, %08x) = %d\n", __func__, inode, mask, ret);
-	KHOOK_PUT(inode_permission);
-
-	return ret;
+	return 1;
 }
-
-////////////////////////////////////////////////////////////////////////////////
-// An example of using KHOOK_EXT
-////////////////////////////////////////////////////////////////////////////////
-
-#include <linux/binfmts.h>
-
-KHOOK_EXT(int, load_elf_binary, struct linux_binprm *);
-static int khook_load_elf_binary(struct linux_binprm *bprm)
-{
-	int ret = 0;
-
-	KHOOK_GET(load_elf_binary);
-	ret = KHOOK_ORIGIN(load_elf_binary, bprm);
-	printk("%s(%p) = %d\n", __func__, bprm, ret);
-	KHOOK_PUT(load_elf_binary);
-
-	return ret;
-}
-
-////////////////////////////////////////////////////////////////////////////////
 
 int init_module(void)
 {
